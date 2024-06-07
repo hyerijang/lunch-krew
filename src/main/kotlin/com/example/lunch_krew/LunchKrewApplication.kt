05671809a95a305897b4083bda6d7a5d2a0d28d3
Hyeri Jang <chjang5621@naver.com>
2024-06-07 23:29:23 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/lunch_krew/LunchKrewApplication.kt
@@ -0,0 +1,11 @@
+package com.example.lunch_krew
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LunchKrewApplication
+
+fun main(args: Array<String>) {
+	runApplication<LunchKrewApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/lunch_krew/LunchKrewApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.lunch_krew
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LunchKrewApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
